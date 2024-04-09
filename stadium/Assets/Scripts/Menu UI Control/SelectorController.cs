4e0909d518628f389499d9565ca0d37df19ada97
Lyndsey Rice <64554773+lrice26@users.noreply.github.com>
2024-04-09 17:17:04 -0500

    Trying...
    Trying to make this work. Messy code that will be cleaned and refined.

--- stadium/Assets/Scripts/Menu UI Control/SelectorController.cs
+++ stadium/Assets/Scripts/Menu UI Control/SelectorController.cs
@@ -15,6 +15,10 @@ public class SelectorController : MonoBehaviour
     public string useSectionName;
     public GameObject CurrentSection;
     public Toggle CurrentToggle;
+    public bool toggleState = false;
+
+    // Note: testing section applier
+    public GameObject[] sectionLEDs;
 
     // Start is called before the first frame update
     void Start()
@@ -51,6 +55,20 @@ public class SelectorController : MonoBehaviour
         // Debug.Log("Lower");
     }
 
+    // Easy way to activate or deactivate sections for section applier
+    public void ToggleCurrentSection()
+    {
+        if (!toggleState)
+        {
+            DeactivateAllElse(useSectionName);
+        }
+        else
+        {
+            ReactivateSections(useSectionName);
+        }
+        toggleState = !toggleState;
+    }
+
     // The section name will be passed externally
     public void ChangeCurrentSection(string sectionName)
     {
@@ -58,32 +76,59 @@ public class SelectorController : MonoBehaviour
         if (GameObject.Find(sectionName) != null)
         {
             CurrentSection = GameObject.Find(sectionName);
+            GetSectionLEDs(sectionName);
         }
-
     }
 
-    // Easy way to activate or deactivate sections for section applier
-    int toggleNumber = 0;
-    public void ToggleCurrentSection()
+    // Update SectionLEDs to hold all the lights - this will be passed to color controller
+    public void GetSectionLEDs(string sectionName)
     {
-        if (toggleNumber == 0)
+        GameObject section = GameObject.Find(sectionName);
+        if (section != null)
         {
-            DeactivateAllElse(useSectionName);
-            toggleNumber = 1;
-        }
-        else
-        {
-            ReactivateSections();
-            toggleNumber = 0;
-        }
+            // Get all children of parent
+            Transform sectionTransform = section.transform;
+
+            // Create a list to store GameObjects with the "LED" tag
+            List<GameObject> LEDList = new List<GameObject>();
+
+            // Loop through each child of the parent GameObject
+            foreach (Transform child in sectionTransform)
+            {
+                // Check if the child has the "LED" tag
+                if (child.CompareTag("LED"))
+                {
+                    // Add the child to the LEDList
+                    LEDList.Add(child.gameObject);
+                }
+            }
+
+            // Convert the list to an array
+            GameObject[] LEDs = LEDList.ToArray();
+
+            // Assign the array to sectionLEDs
+            sectionLEDs = LEDs;
+
+        } 
     }
+
 
     public void DeactivateAllElse(string currentSection)
     {
-        if (currentSection != null)
+        if (currentSection.ToLower().Contains("low"))
         {
+            ToggleTopDeck();
+            ToggleUpperDeck();
             DeactivateSection("Lower Deck", currentSection);
+        } else if (currentSection.ToLower().Contains("upper"))
+        {
+            ToggleTopDeck();
+            ToggleLowerDeck();
             DeactivateSection("Upper Deck", currentSection);
+        } else
+        {
+            ToggleUpperDeck();
+            ToggleLowerDeck();
             DeactivateSection("Top Deck", currentSection);
         }
     }
@@ -104,11 +149,26 @@ public class SelectorController : MonoBehaviour
         
     }
 
-    public void ReactivateSections()
+    public void ReactivateSections(string currentSection)
     {
-        ReactivateSection("Lower Deck");
-        ReactivateSection("Upper Deck");
-        ReactivateSection("Top Deck");
+        if (currentSection.ToLower().Contains("low"))
+        {
+            ToggleTopDeck();
+            ToggleUpperDeck();
+            ReactivateSection("Lower Deck");
+        }
+        else if (currentSection.ToLower().Contains("upper"))
+        {
+            ToggleTopDeck();
+            ToggleLowerDeck();
+            ReactivateSection("Upper Deck");
+        }
+        else
+        {
+            ToggleUpperDeck();
+            ToggleLowerDeck();
+            ReactivateSection("Top Deck");
+        }
     }
 
     private void ReactivateSection(string sectionName)
